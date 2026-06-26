317190a6304d8d6c6c075713d01a93b1686b9740
CyberAxe3108 <crismaramaro31@gmail.com>
2026-06-26 15:10:03 -0400

    phase 2: textContent, classList, and attribute methods

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ console.log(genreInput);
 console.log(movieList);
 console.log(clearWatchedBtn);
 console.log(filterBtns);
+appTitle.textContent = "My Movie Watchlist"
+
+// Read and log the current count text
+console.log("Count says:", movieCount.textContent)
+
+// Update the count text manually (JavaScript will keep this accurate later)
+movieCount.textContent = "0 movies"
+
+movieCount.classList.add("active-filter")
+movieCount.classList.remove("active-filter")
+movieCount.classList.toggle("active-filter")
+movieCount.classList.toggle("active-filter")
+
+
+console.log(titleInput.getAttribute("placeholder")) 
+console.log(titleInput.getAttribute("type"))         
+console.log(titleInput.getAttribute("required"))
+titleInput.setAttribute("placeholder", "Try: The Matrix")
+titleInput.removeAttribute("required")
+titleInput.setAttribute("required", "")
+titleInput.getAttribute("value")  
+titleInput.value     
+// What is the difference between getAttribute("value") and .value on an input?
+// getAttribute("value") → This reads what was written in the HTML file
+// .value               →  This reads what the user typed at the moment.
